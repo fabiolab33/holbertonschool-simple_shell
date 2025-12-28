e40979b73f675999dc689cbd8f28db80f4d408bb
fabiola <fabiola@penguin>
2025-12-27 20:46:38 -0700

    handle command lines

--- shell.c
+++ shell.c
@@ -10,17 +10,17 @@
  */
 static int build_args(char *line, char **args)
 {
-	int i = 0;
-	char *token;
+int i = 0;
+char *token;
 
-	token = strtok(line, " \t");
-	while (token != NULL && i < (MAX_ARGS - 1))
-	{
-		args[i++] = token;
-		token = strtok(NULL, " \t");
-	}
-	args[i] = NULL;
-	return (i);
+token = strtok(line, " \t");
+while (token != NULL && i < (MAX_ARGS - 1))
+{
+args[i++] = token;
+token = strtok(NULL, " \t");
+}
+args[i] = NULL;
+return (i);
 }
 
 /**
@@ -33,37 +33,37 @@ static int build_args(char *line, char **args)
  */
 int execute_command(char **args, char *name, unsigned int line_number)
 {
-	pid_t pid;
-	int status;
+pid_t pid;
+int status;
 
-	if (args == NULL || args[0] == NULL)
-		return (0);
+if (args == NULL || args[0] == NULL)
+return (0);
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		return (1);
-	}
-	else if (pid == 0)
-	{
-		/* Child process: execute the command */
-		if (execve(args[0], args, environ) == -1)
-		{
-			fprintf(stderr, "%s: %u: %s: not found\n",
-				name, line_number, args[0]);
-			exit(127);
-		}
-	}
-	else
-	{
-		/* Parent process: wait for child to complete */
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-	}
+pid = fork();
+if (pid == -1)
+{
+perror("fork");
+return (1);
+}
+else if (pid == 0)
+{
+/* Child process: execute the command */
+if (execve(args[0], args, environ) == -1)
+{
+fprintf(stderr, "%s: %u: %s: not found\n",
+name, line_number, args[0]);
+exit(127);
+}
+}
+else
+{
+/* Parent process: wait for child to complete */
+waitpid(pid, &status, 0);
+if (WIFEXITED(status))
+return (WEXITSTATUS(status));
+}
 
-	return (0);
+return (0);
 }
 
 /**
@@ -74,47 +74,48 @@ int execute_command(char **args, char *name, unsigned int line_number)
  */
 int shell_loop(char *name)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	int interactive = isatty(STDIN_FILENO);
-	unsigned int line_number = 0;
-	int last_status = 0;
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
+int interactive = isatty(STDIN_FILENO);
+unsigned int line_number = 0;
+int last_status = 0;
 
-	while (1)
-	{
-		char *args[MAX_ARGS];
-		int argc;
+while (1)
+{
+char *args[MAX_ARGS];
+int argc;
 
-		if (interactive)
-			write(STDOUT_FILENO, PROMPT, sizeof(PROMPT) - 1);
+if (interactive)
+write(STDOUT_FILENO, PROMPT, sizeof(PROMPT) - 1);
 
-		nread = getline(&line, &len, stdin);
+nread = getline(&line, &len, stdin);
 
-		/* EOF (Ctrl+D) */
-		if (nread == -1)
-		{
-			if (interactive)
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
+/* EOF (Ctrl+D) */
+if (nread == -1)
+{
+if (interactive)
+write(STDOUT_FILENO, "\n", 1);
+break;
+}
 
-		line_number++;
+line_number++;
 
-		/* strip newline */
-		if (nread > 0 && line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+/* strip newline */
+if (nread > 0 && line[nread - 1] == '\n')
+line[nread - 1] = '\0';
 
-		/* tokenize into argv */
-		argc = build_args(line, args);
+/* tokenize into argv */
+argc = build_args(line, args);
 
-		/* empty / spaces-only line */
-		if (argc == 0)
-			continue;
+/* empty / spaces-only line */
+if (argc == 0)
+continue;
 
-		last_status = execute_command(args, name, line_number);
-	}
+last_status = execute_command(args, name, line_number);
+}
 
-	free(line);
-	return (last_status);
+free(line);
+return (last_status);
+>>>>>>> 0e2c4b0 (Handle command lines)
 }
