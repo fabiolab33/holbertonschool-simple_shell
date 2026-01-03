82f9790082d211b2e8c651dc50a814aefaf84c97
Fabiola Burgos <fburgos913@gmail.com>
2026-01-03 01:05:21 -0700

    Handle the PATH

--- execute_command.c
+++ execute_command.c
@@ -1,6 +1,53 @@
 #include "shell.h"
 
 /**
+ * get_full_path - search PATH for executable
+ * @cmd: command name
+ *
+ * Return: pointer to full path (static buffer), or NULL if not found
+ */
+char *get_full_path(char *cmd)
+{
+    static char full_path[PATH_MAX_LEN];
+    char *path_env, *dir, *path_copy;
+
+    if (cmd == NULL)
+        return (NULL);
+
+    if (cmd[0] == '/' || cmd[0] == '.')
+    {
+        if (access(cmd, X_OK) == 0)
+            return cmd;
+        else
+            return NULL;
+    }
+
+    path_env = getenv("PATH");
+    if (!path_env)
+        return NULL;
+
+    path_copy = strdup(path_env);
+    if (!path_copy)
+        return NULL;
+
+    dir = strtok(path_copy, ":");
+    while (dir != NULL)
+    {
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_copy);
+            return full_path;
+        }
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return NULL;
+}
+#include "shell.h"
+
+/**
  * execute_command - Execute a command with arguments
  * @args: Array of command arguments (NULL-terminated)
  * @name: Program name (argv[0]) for error messages
