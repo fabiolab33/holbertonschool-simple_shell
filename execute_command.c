62e969a5351bb4f2f0757947fc63e9b52b7fc17e
Fabiola Burgos <fburgos913@gmail.com>
2026-01-09 14:19:39 -0400

    Handle the PATH

--- execute_command.c
+++ execute_command.c
@@ -8,10 +8,33 @@
 #include <sys/stat.h>
 
 /**
- * get_full_path - search PATH for executable
- * @cmd: command name
+ * _getenv - get environment variable value without using getenv
+ * @name: variable name to find
  *
- * Return: pointer to full path (static buffer), or NULL if not found
+ * Return: pointer to value, or NULL if not found
+ */
+char *_getenv(char *name)
+{
+int i = 0;
+size_t name_len;
+
+if (!name || !environ)
+return (NULL);
+name_len = strlen(name);
+while (environ[i])
+{
+if (strncmp(environ[i], name, name_len) == 0 &&
+environ[i][name_len] == '=')
+return (environ[i] + name_len + 1);
+i++;
+}
+return (NULL);
+}
+/**
+ * get_full_path - search PATH efor executable
+ * @cmd: Command name
+ *
+ * Return: pointer to  full path (static buffer), or NULL if not found
  */
 char *get_full_path(char *cmd)
 {
@@ -19,37 +42,32 @@ static char full_path[PATH_MAX_LEN];
 char *path_env, *dir, *path_copy;
 
 if (!cmd)
-return NULL;
-
+return (NULL);
 if (cmd[0] == '/' || cmd[0] == '.')
 {
 if (access(cmd, X_OK) == 0)
-return cmd;
-return NULL;
+return (cmd);
+return (NULL);
 }
-
-path_env = getenv("PATH");
+path_env = _getenv("PATH");
 if (!path_env)
-return NULL;
-
+return (NULL);
 path_copy = strdup(path_env);
 if (!path_copy)
-return NULL;
-
+return (NULL);
 dir = strtok(path_copy, ":");
 while (dir)
 {
 snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
 if (access(full_path, X_OK) == 0)
 {
-free(path_copy);
-return full_path;
+ free(path_copy);
+return (full_path);
 }
 dir = strtok(NULL, ":");
 }
-
 free(path_copy);
-return NULL;
+return (NULL);
 }
 
 /**
@@ -74,14 +92,14 @@ if (!cmd_path)
 {
 dprintf(STDERR_FILENO, "%s: %u: %s: not found\n",
 name, line_number, args[0]);
-return 127;
+return (127);
 }
 
 pid = fork();
 if (pid == -1)
 {
 perror("fork");
-return 1;
+return (1);
 }
 else if (pid == 0) /* child */
 {
@@ -97,14 +115,14 @@ else /* parent */
 if (waitpid(pid, &status, 0) == -1)
 {
 perror("waitpid");
-return 1;
+return (1);
 }
 
 if (WIFEXITED(status))
 return WEXITSTATUS(status);
 else
-return 1;
+return (1);
 }
 
-return 0;
+return (0);
 }
